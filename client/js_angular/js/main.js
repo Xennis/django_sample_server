6932c7510bf89cc9ff50873b9c0991542060316c
Xennis <xennis@online.de>
2014-12-19 13:29:15 +0100

    Angular client: update Anglar version, make use of scope

--- client/js_angular/js/main.js
+++ client/js_angular/js/main.js
@@ -1,43 +1,37 @@
 (function(){
 	var app = angular.module('exampleApp', ['HttpClient']);
 
-	app.controller('SampleAPIController', ['httpClient', function(httpClient){
-		var _self = this;
-
-		_self.API_URL = "http://localhost:8000/api/v1/";
+	app.controller('SampleAPIController', ['$scope', 'httpClient', function($scope, httpClient){
+		$scope.API_URL = "http://localhost:8000/api/v1/";
 		
-		_self.access_token = "33faa76c33b5f6fdef888fda0ca379a5deecc739";
+		$scope.access_token = "33faa76c33b5f6fdef888fda0ca379a5deecc739";
 
-		_self.locations = [];
-		_self.getLocationsResult = '';
-		_self.postLocationResult = '';
-		
-		_self.postLocation = function() {
+		$scope.postLocation = function() {
 			var location = JSON.stringify({
-				user_id: 2,
-				latitude: _self.postLatitude,
-				longitude: _self.postLongitude,
-				altitude: _self.postAltitude
+				user_id: 10,
+				latitude: $scope.postLatitude,
+				longitude: $scope.postLongitude,
+				altitude: $scope.postAltitude
 			});
 
-			httpClient.httpPost(_self.API_URL + 'location/', 'OAuth ' + _self.access_token, location,
+			httpClient.httpPost($scope.API_URL + 'location/', 'OAuth ' + $scope.access_token, location,
 				function (status) {
-					_self.postLocationResult = status;
+					$scope.postLocationResult = status;
 				},
 				function (status, errorMessage) {
-					_self.postLocationResult = status + ' (' + errorMessage + ')';
+					$scope.postLocationResult = status + ' (' + errorMessage + ')';
 				}
 			);
 		};
 
-		_self.getLocations = function() {
-			httpClient.httpGet(_self.API_URL + 'location/', 'OAuth ' + _self.access_token,
+		$scope.getLocations = function() {
+			httpClient.httpGet($scope.API_URL + 'location/', 'OAuth ' + $scope.access_token,
 				function (data, status) {
-					_self.locations = data.objects;
-					_self.getLocationsResult = status;
+					$scope.locations = data.objects;
+					$scope.getLocationsResult = status;
 				},
 				function (status, errorMessage) {
-					_self.getLocationsResult = status + ' (' + errorMessage + ')';
+					$scope.getLocationsResult = status + ' (' + errorMessage + ')';
 				}
 			);
 		};
